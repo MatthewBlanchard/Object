a2301b0551dd073da18e82b03cdcb58b682d5f7a
Matthew Blanchard <matthewblanc@msn.com>
2012-10-29 01:48:49 +0000

    Stops global namespace pollution.

--- Minified/Tiny.lua
+++ Minified/Tiny.lua
@@ -1,9 +1,8 @@
-z={} _s=setmetatable
+local z,_s={},setmetatable
 _s(z,z)function z.__call(s, ...)return s:new(...)end
 function z:init()end
 function z.parent()return getmetatable(s)end
-function z.new(s,...)local o=_s({},s)s.__index=s
-s.__call=Object.__call
+function z.new(s,...)local o=_s({},s)s.__index,s.__call=s,Object.__call
 o:init(...)return o
 end
 Object=z
