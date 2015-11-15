46cbca7840c5cc90571c71b1ac70c63d7ee60a95
Matthew Blanchard <matthewblanc@msn.com>
2015-11-15 01:36:09 -0500

    Simplified base object

--- object.lua
+++ object.lua
@@ -1,11 +1,5 @@
 Object = {}
 
--- Variables
-Object.__metamethods =  {
-    "__add", "__sub", "__mul", "__div", "__mod", "__pow", "__unm",
-    "__len", "__lt", "__le", "__concat", "__tostring"
-}
-
 -- Metamethods
 function Object:__call(...)
     return self:new(...)
@@ -15,36 +9,22 @@ end
 function Object:__new()
 end
 
--- Private/Static methods
-function Object:__metamethod(event)
-    return function(...)
-        func = self[event]
-        
-        if type(func) == "function" then 
-            return func(...) 
-        else
-            return func
-        end
-    end
+-- Static Methods
+function Object.__cast(o, m)
+	setmetatable(o, m)
+	m.__index = m
+	m.__call = Object.__call
+
+	return o
 end
 
 -- Methods
-function Object:parent()
-    return getmetatable(self)
-end
-
 function Object:new(...)
     local o = {}
-    setmetatable(o, self)
-    self.__index = self
-    self.__call = Object.__call
-    
-    for k,v in pairs(Object.__metamethods) do
-        o[v] = self:__metamethod(v)
-    end
-    
-    local err = o:__new(...)
-    return err or o
+   	Object.__cast(o, self)
+
+    o:__new(...)
+    return o
 end
 
 Object = Object:new()
